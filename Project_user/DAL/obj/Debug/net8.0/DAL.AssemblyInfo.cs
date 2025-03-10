cfead544c9cc3423c999aba3c209c5cdcdfbeb09
Bảo Anh <nhubaoanh111@gmail.com>
2025-03-10 10:10:52 +0700

    Bản back-up csdl

--- Project_user/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ Project_user/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36936619c74c77635bc6848a0df5610181b6180b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
